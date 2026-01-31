79e467a5a670c09539aaa7e7ddc0b70752fdda7f
nicolacentonze <nicola.centonze@icloud.com>
2026-01-31 22:26:16 +0100

    Init list prototype

--- demo/App.vue
+++ demo/App.vue
@@ -8,6 +8,7 @@ import {
 	NuitralTab,
 	NuitralTabs,
 	NuitralChip,
+	NuitralList,
 	useNuitralTheming,
 } from '../lib'
 import { ref } from 'vue'
@@ -48,6 +49,13 @@ const testButton = () => {
 	<button @click="manageTheme('pop')">Set pop theme</button>
 	<h1>nuitral Vue UI Suite</h1>
 
+	<div class="mb-2">
+		<NuitralList>
+			<template v-slot:start>A</template>
+			a
+			<template v-slot:end>B</template>
+		</NuitralList>
+	</div>
 	<br />
 	<div class="flex gap-2">
 		<NuitralChip primary icon="basket">Chip Primary</NuitralChip>
---
+++ lib/components/list/List.vue
@@ -0,0 +1,60 @@
+<script lang="ts" setup>
+import { computed, useSlots } from 'vue'
+
+interface ListProps {
+	direction: 'horizontal' | 'vertical'
+	classes: string[] | string
+}
+
+const props = withDefaults(defineProps<ListProps>(), {
+	direction: 'horizontal',
+	classes: '',
+})
+
+const computedClasses = computed(() => `${props.classes} ${props.direction}`)
+
+const slots = useSlots()
+</script>
+<template>
+	<div class="nuitral-list" :class="[computedClasses]">
+		<div class="start">
+			<slot name="start" v-if="slots.start" />
+		</div>
+		<div class="default">
+			<slot />
+		</div>
+		<div class="end">
+			<slot name="end" v-if="slots.end" />
+		</div>
+	</div>
+</template>
+
+<style lang="scss">
+.nuitral-list {
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+
+	&.horizontal {
+		flex-direction: row;
+		height: 100%;
+	}
+
+	&.vertical {
+		flex-direction: column;
+		width: 100%;
+	}
+
+	.start,
+	.end {
+		flex: 0 0 auto;
+	}
+
+	.default {
+		flex: 1;
+		display: flex;
+		align-items: center;
+		justify-content: start;
+	}
+}
+</style>
