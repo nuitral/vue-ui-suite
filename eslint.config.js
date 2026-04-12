ea46f07032c22f1f8f9b6c9afeec738d33f84aeb
nicolacentonze <nicola.centonze@icloud.com>
2026-04-12 22:29:15 +0200

    New eslint config - package improvements

--- eslint.config.js
+++ eslint.config.js
@@ -6,41 +6,25 @@ import { defineConfig } from 'eslint/config'
 import stylistic from '@stylistic/eslint-plugin'
 
 export default defineConfig([
-    {
-        files: ['**/*.{js,mjs,cjs,ts,mts,cts,vue}'],
-        plugins: { js },
-        extends: ['js/recommended'],
-        languageOptions: { globals: globals.browser },
-    },
+    js.configs.recommended,
     {
         ignores: ['node_modules', 'dist'],
     },
-    tseslint.configs.recommended,
-    ...pluginVue.configs['flat/essential'],
     {
-        rules: {
-            'vue/multi-word-component-names': 'off',
-            '@typescript-eslint/no-explicit-any': 'off',
-            'vue/no-use-v-if-with-v-for': 'off',
-            'vue/require-valid-default-prop': 'off',
-            'vue/no-deprecated-slot-attribute': 'off',
-            semi: ['error', 'never'],
-            'no-extra-semi': 'error',
-            '@stylistic/semi': 'off',
-            'vue/max-attributes-per-line': [
-                'error',
-                {
-                    singleline: 4,
-                    multiline: 1,
-                },
-            ],
-            'vue/html-indent': ['error', 4],
-            '@stylistic/quotes': ['error', 'single', { avoidEscape: true }],
+        files: ['**/*.{js,mjs,cjs,ts,mts,cts,vue}'],
+        languageOptions: {
+            globals: globals.browser,
         },
     },
+    ...tseslint.configs.recommended,
+    ...pluginVue.configs['flat/essential'],
     {
         files: ['**/*.vue'],
-        languageOptions: { parserOptions: { parser: tseslint.parser } },
+        languageOptions: {
+            parserOptions: {
+                parser: tseslint.parser,
+            },
+        },
     },
     {
         plugins: {
@@ -48,6 +32,31 @@ export default defineConfig([
         },
         rules: {
             '@stylistic/indent': ['error', 4],
+            '@stylistic/quotes': ['error', 'single', { avoidEscape: true }],
+        },
+    },
+
+    {
+        rules: {
+            'vue/multi-word-component-names': 'off',
+            '@typescript-eslint/no-explicit-any': 'off',
+            'vue/no-use-v-if-with-v-for': 'off',
+            'vue/require-valid-default-prop': 'off',
+            'vue/no-deprecated-slot-attribute': 'off',
+
+            semi: ['error', 'never'],
+            'no-extra-semi': 'error',
+            '@stylistic/semi': 'off',
+
+            'vue/max-attributes-per-line': [
+                'error',
+                {
+                    singleline: 4,
+                    multiline: 1,
+                },
+            ],
+
+            'vue/html-indent': ['error', 4],
         },
     },
 ])
